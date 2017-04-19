349c551925a70b4e69ea318d3ed4d0922f9d8cc5
E Pogrebnyak <e.pogrebnyak@gmail.com>
2017-04-19 13:00:52 +0300

    dataset ready

--- ust.py
+++ ust.py
@@ -310,9 +310,11 @@ http://data.treasury.gov/Feed.svc/DailyTreasuryYieldCurveRateData(6829)
 </entry>
 </pre>"""
 
+import os
+from datetime import datetime
 import requests 
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 
 check_point = dict(date="2017-04-03"
@@ -329,26 +331,51 @@ check_point = dict(date="2017-04-03"
  , BC_30YEARDISPLAY = 2.98) 
 
 BC_KEYS = ['BC_3MONTH', 'BC_6MONTH',  'BC_1YEAR',  'BC_2YEAR', 'BC_3YEAR', 
-            'BC_5YEAR',  'BC_7YEAR', 'BC_10YEAR', 'BC_20YEAR', 'BC_30YEAR', 
-            'BC_30YEARDISPLAY'] 
+           'BC_5YEAR',  'BC_7YEAR',  'BC_10YEAR', 'BC_20YEAR', 'BC_30YEAR', 
+           'BC_30YEARDISPLAY'] 
 DF_COLUMNS = ['date'] + BC_KEYS 
 
+def get_date(string):
+    dt = datetime.strptime(string, '%Y-%m-%dT%H:%M:%S')
+    return dt.strftime('%Y-%m-%d')
+    
+def as_float(s: str):
+    # work around 2002 omissions in 30yr data
+    try: 
+        x = float(s)
+        return x
+    except:
+        return 0
+    
 
-def yield_datapoints(xml_content: str)-> list:
+def yield_datapoints_from_string(xml_content: str)-> iter:
     """Parse XML string and yield one dictionary per date."""
     soup = BeautifulSoup(xml_content,'xml')
     data = soup.find_all('content')
     for datum in data:        
-        cur_dict = dict((key, float(datum.find(key).text)) for key in BC_KEYS)
-        # FIXME LOW: use datetime to collect date, .text[:10] is a stub 
-        date_dict = dict(date=datum.find('NEW_DATE').text[:10])
-        # end FIXME ---
-        cur_dict.update(date_dict)  
+        cur_dict = dict((key, as_float(datum.find(key).text)) for key in BC_KEYS)
+        cur_dict['date'] = get_date(datum.find('NEW_DATE').text)
         yield(cur_dict)
-
+        
+# def get_datapoints(xml_content):
+    # # placeholder for parser code
+    # soup = BeautifulSoup(xml_content, 'xml')
+    # properties = soup.find_all('properties')
+    # checkpoints = []
+    # for prop in properties:
+        # children = prop.findChildren()
+        # checkpoint = {}
+        # checkpoint_date = datetime.strptime(children[1].text, '%Y-%m-%dT%H:%M:%S')
+        # checkpoint['date'] = checkpoint_date.strftime('%Y-%m-%d')
+        # children = children[3:]  # all the BCs
+        # for child in children:
+            # checkpoint[child.name] = float(child.text)
+        # checkpoints.append(checkpoint)        
+        
+        
 # DONE: extract a list of dictionaries like *check_point* from XML_CONTENT
 #       using BeautifulSop and/or ElementTree in Python 3.
-d = list(yield_datapoints(xml_content = XML_CONTENT))
+d = list(yield_datapoints_from_string(xml_content = XML_CONTENT))
 assert d[0] == check_point #get_datapoints(XML_CONTENT)[0] == check_point
 assert len(d) == 10 #get_datapoints(XML_CONTENT)) == 10                    
     
@@ -358,32 +385,93 @@ def get_url(year: int)-> str:
 assert get_url(2017) == "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/pages/XmlView.aspx?data=yieldyear&year=2017"
 
 
-def foo(year: int, force=True):
+# --------------------------------------------------------------
+#
+# Local XML file cache
+#
+# --------------------------------------------------------------
+
+
+def filepath(year: int):
+    dirname = 'xml'
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)        
+    fn = "{}.xml".format(year)
+    return os.path.join(dirname, fn)
+    
+def read_local_xml(year):
+    path = filepath(year)
+    with open(path, 'r') as f:
+        return f.read()
+    
+def save_local_xml(year: int, content: str):
+    path = filepath(year)
+    with open(path, "w") as f:
+        f.write(content)
+
+# end of cache -----------------------------------------------------    
+
+def get_xml_content_from_web(year: int)-> str:
+    """Returns XML content as string"""
     url = get_url(year) 
-    if force:
-        r = requests.get(url)
+    r = requests.get(url)
+    content = r.text
+    if "Error" in content:
+        # when calling API too error emerges. Should not be a problem with local files. 
+        raise ValueError("Cannot read from web. Try again later.")
     else:
-        pass
-        # value error
-    return yield_datapoints(r.content)
+        return content
+ 
+def get_datapoints(year: int, from_web=False):
+    if os.path.exists(filepath(year)) and not from_web:        
+        xml_content = read_local_xml(year)
+        print("Read {} data from local file.".format(year))
+    else:
+        xml_content = get_xml_content_from_web(year)
+        save_local_xml(year, xml_content)
+        print("Read {} data from web and saved to local file.".format(year))
+    return yield_datapoints_from_string(xml_content)
 
-# FIXME: 2017 is dattime current year
-YEARS = [2016] #[x for x in range(1990,2017+1)]
+# WARNING: calling API too often causes temporary error
+#content = get_xml_content_from_web(2017)
+#pts = list(yield_datapoints_from_string(content))
+#assert check_point in pts
+       
 
-import itertools
+def get_df(year):
+    gen = get_datapoints(year)
+    df = pd.DataFrame(gen)[DF_COLUMNS]
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace = True)
+    return df
+    
+def to_monthly_average(df: pd.DataFrame):
+    mf = df.resample("M", how = 'mean').round(2)
+    # line below does averaging on wrong axis
+    # mf = df.resample("M").mean().round(2)
+    mf.insert(0, "month", mf.index.month)
+    mf.insert(0, "year", mf.index.year)
+    return mf
+    
+if __name__ == "__main__":
+    cur_year = datetime.today().year
+    YEARS = [x for x in range(1990,cur_year+1)]
+    # daily data
+    dfs = [get_df(year) for year in  YEARS]
+    df = pd.concat(dfs)    
+    df.to_excel("ust_daily.xlsx")
+    # monthly averages
+    mf = to_monthly_average(df)        
+    mf.to_excel("ust_month_average.xlsx")
+    
 
-def foo2():
-    return itertools.chain(foo(year) for year in YEARS)
-           
-#TODO: get_df(year)  
+#TODO 1: comment code, where unclear or doubtfuk
+#TODO 2: separate tests from code, write tests as unittest or pytest
 
-import pandas as pd
-z = foo2()
-df = pd.DataFrame(*z)[DF_COLUMNS]
-df['date'] = pd.to_datetime(df['date'])
-df.set_index('date', inplace = True)
-df.to_excel("ust_daily.xlsx")
-mf = df.resample("M").mean().round(2)
-mf.insert(0, "month", mf.index.month)
-mf.insert(0, "year", mf.index.year)
-mf.to_excel("ust_month_average.xlsx")
+#MAYBE / NOT TODO: daily updates:
+#        - save dfs to csv 
+#        - read last year df
+#        - add to dataframe
+#        - dump output files
+
+#MAYBE / NOT TODO: read all-time xml
